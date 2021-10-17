f936bdc316acb6954f0ddbaa058956f2bb647de8
darsh-patel <70541374+darsh-patel@users.noreply.github.com>
2021-10-17 16:04:40 -0400

    Added helper functions necessary for GaussFSSD GoF test

--- darsh_patel/figure_code/kgof/util.py
+++ darsh_patel/figure_code/kgof/util.py
@@ -34,3 +34,84 @@ class NumpySeedContext(object):
         np.random.set_state(self.cur_state)
 
 # end NumpySeedContext
+
+def dist_matrix(X, Y):
+    """
+    Construct a pairwise Euclidean distance matrix of size X.shape[0] x Y.shape[0]
+    """
+    sx = np.sum(X**2, 1)
+    sy = np.sum(Y**2, 1)
+    D2 =  sx[:, np.newaxis] - 2.0*X.dot(Y.T) + sy[np.newaxis, :] 
+    # to prevent numerical errors from taking sqrt of negative numbers
+    D2[D2 < 0] = 0
+    D = np.sqrt(D2)
+    return D
+
+def meddistance(X, subsample=None, mean_on_fail=True):
+    """
+    Compute the median of pairwise distances (not distance squared) of points
+    in the matrix.  Useful as a heuristic for setting Gaussian kernel's width.
+    Parameters
+    ----------
+    X : n x d numpy array
+    mean_on_fail: True/False. If True, use the mean when the median distance is 0.
+        This can happen especially, when the data are discrete e.g., 0/1, and 
+        there are more slightly more 0 than 1. In this case, the m
+    Return
+    ------
+    median distance
+    """
+    if subsample is None:
+        D = dist_matrix(X, X)
+        Itri = np.tril_indices(D.shape[0], -1)
+        Tri = D[Itri]
+        med = np.median(Tri)
+        if med <= 0:
+            # use the mean
+            return np.mean(Tri)
+        return med
+
+    else:
+        assert subsample > 0
+        rand_state = np.random.get_state()
+        np.random.seed(9827)
+        n = X.shape[0]
+        ind = np.random.choice(n, min(subsample, n), replace=False)
+        np.random.set_state(rand_state)
+        # recursion just one
+        return meddistance(X[ind, :], None, mean_on_fail)
+
+def is_real_num(X):
+    """return true if x is a real number. 
+    Work for a numpy array as well. Return an array of the same dimension."""
+    def each_elem_true(x):
+        try:
+            float(x)
+            return not (np.isnan(x) or np.isinf(x))
+        except:
+            return False
+    f = np.vectorize(each_elem_true)
+    return f(X)
+
+def fit_gaussian_draw(X, J, seed=28, reg=1e-7, eig_pow=1.0):
+    """
+    Fit a multivariate normal to the data X (n x d) and draw J points 
+    from the fit. 
+    - reg: regularizer to use with the covariance matrix
+    - eig_pow: raise eigenvalues of the covariance matrix to this power to construct 
+        a new covariance matrix before drawing samples. Useful to shrink the spread 
+        of the variance.
+    """
+    with NumpySeedContext(seed=seed):
+        d = X.shape[1]
+        mean_x = np.mean(X, 0)
+        cov_x = np.cov(X.T)
+        if d==1:
+            cov_x = np.array([[cov_x]])
+        [evals, evecs] = np.linalg.eig(cov_x)
+        evals = np.maximum(0, np.real(evals))
+        assert np.all(np.isfinite(evals))
+        evecs = np.real(evecs)
+        shrunk_cov = evecs.dot(np.diag(evals**eig_pow)).dot(evecs.T) + reg*np.eye(d)
+        V = np.random.multivariate_normal(mean_x, shrunk_cov, J)
+    return V
